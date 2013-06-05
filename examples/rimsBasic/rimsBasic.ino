e5359c887b4db740f852bdbb33c5e7e190b54376
Francis Gagnon <Francis Gagnon@localhost>
2013-06-05 17:18:06 -0400

    -modif rimsBasic -add identRimsBasic

--- examples/rimsBasic/rimsBasic.ino
+++ examples/rimsBasic/rimsBasic.ino
@@ -1,21 +1,20 @@
-#include <LiquidCrystal.h>
-#include "utility/UIRims.h"
+
+#include "LiquidCrystal.h"
 #include "Rims.h"
 
-double settedTemp, currentTemp, ssrControl;
+double currentTemp, ssrControl, settedTemp;
 
 LiquidCrystal lcd(8,9,4,5,6,7);
 UIRims myUI(lcd,16,2,10,0);
-Rims myRims(myUI,1,11,&currentTemp,&ssrControl,&settedTemp);
+Rims myRims(&myUI,1,11,&currentTemp,&ssrControl,&settedTemp);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  myRims.setLedPin(13);
+  myRims.setPinLED(13);
   myRims.setInterruptFlow(1);
-  myRims.setTunningPID(1,1,1,10);
-
+  myRims.setTunningPID(2,5,1,0.1);
 }
 void loop() {
   myRims.start();
-};
+}
