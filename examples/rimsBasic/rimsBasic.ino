97490e63771a67d2dad5984f7d0bda252c0d02ff
Francis Gagnon <Francis Gagnon@localhost>
2013-06-16 23:52:37 -0400

    -modif basicRims

--- examples/rimsBasic/rimsBasic.ino
+++ examples/rimsBasic/rimsBasic.ino
@@ -5,16 +5,14 @@
 double currentTemp, ssrControl, settedTemp;
 
 LiquidCrystal lcd(8,9,4,5,6,7);
-UIRims myUI(lcd,16,2,0,10);
+UIRims myUI(&lcd,0,10);
 Rims myRims(&myUI,1,11,&currentTemp,&ssrControl,&settedTemp);
 
 void setup() {
   // put your setup code here, to run once:
-  myRims.setPinLED(13);
-  myRims.setInterruptFlow(1,9.0);
   myRims.setTunningPID(2,5,1,0.1);
   myRims.setSetPointFilter(1);
 }
 void loop() {
-  myRims.start();
+  myRims.run();
 }
