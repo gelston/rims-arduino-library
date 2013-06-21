05c269a4e1b5eafa066696bfd3f3463cef9e7e95
Francis Gagnon <Francis Gagnon@localhost>
2013-06-21 18:39:33 -0400

    -docu html revert

--- html/search/all_75.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['uirims',['UIRims',['../class_u_i_rims.html',1,'']]],
-  ['uirimsident',['UIRimsIdent',['../class_u_i_rims_ident.html',1,'']]]
-];
--- html/search/classes_70.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pid',['PID',['../class_p_i_d.html',1,'']]]
-];
